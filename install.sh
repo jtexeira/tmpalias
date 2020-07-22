2f50cf826fd7143e02baffd7ba834ed387a5de83
hitler <a85504@alunos.uminho.pt>
2020-07-22 12:06:41 +0100

    add install path

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
+#!/bin/sh
+BRC=~/.bashrc
+case $1 in
+    -p)
+        BRC=$2
+esac
 sudo cp tmpalias.sh /usr/local/bin/tmpalias
 sudo chmod +x /usr/local/bin/tmpalias
 touch ~/.tmpalias
-echo '. ~/.tmpalias' >> ~/.bashrc
+echo '. ~/.tmpalias' >> $BRC
